8380cfe2576b7396176a901e4f37e6f16298d10c
lbk00 <lbk6661@gmail.com>
2025-08-25 16:27:55 +0900

    feat : 실행시간 로깅 추가

--- dags/user_summary_dag.py
+++ dags/user_summary_dag.py
@@ -14,6 +14,11 @@ import os
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path)
 
+print("DB_HOST:", os.environ.get("DB_HOST"))
+print("DB_PORT:", os.environ.get("DB_PORT"))
+print("DB_NAME:", os.environ.get("DB_NAME"))
+print("DB_USERNAME:", os.environ.get("DB_USERNAME"))
+print("DB_PASSWORD:", os.environ.get("DB_PASSWORD"))
 
 def extract_list_or_single(value):
     if isinstance(value, list):
